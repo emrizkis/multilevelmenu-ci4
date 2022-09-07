8c2a75db1a207f97a21c9695e950342891a97cb6
emrizkis <emrizkis@gmail.com>
2022-09-07 10:35:06 +0700

    first commit

---
+++ config.php
@@ -0,0 +1,39 @@
+<?php
+
+// $config["menu_id"]               = 'id';
+// $config["menu_label"]            = 'name';
+// $config["menu_parent"]           = 'parent';
+// $config["menu_icon"] 			 = 'icon';
+$config["menu_key"]              = 'slug';
+$config["menu_order"]            = 'number';
+
+$config["nav_tag_open"]          = '<ul>';
+$config["nav_tag_close"]         = '</ul>';
+$config["item_tag_open"]         = '<li>';
+$config["item_tag_close"]        = '</li>';
+$config["parent_tag_open"]       = '<li>';
+$config["parent_tag_close"]      = '</li>';
+$config["parent_anchor_tag"]     = '<a href="%s">%s</a>';
+$config["children_tag_open"]     = '<ul>';
+$config["children_tag_close"]    = '</ul>';
+$config['icon_position']         = 'left'; // 'left' or 'right'
+$config['menu_icons_list']         = array();
+// these for the future version
+$config['icon_img_base_url']     = '';
+
+
+// for bootstrap 4
+
+// $config["nav_tag_open"]             = '<ul class="navbar-nav">';
+
+// $config["parent_tag_open"]          = '<li class="nav-item dropdown dropdown-hover ml-1">';
+// $config["parent_anchor"]            = '<a data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="dropdown-item dropdown-toggle" href="%s">%s</a>';
+// $config["item_anchor"]              = '<a role:"button" class="dropdown-item" href="%s">%s</a>';
+
+// $config["parent_header_tag_open"]   = '<li class="nav-item dropdown dropdown-submenu dropdown-hover ml-1">';
+
+// $config["children_tag_open"]        = '<ul class="dropdown-menu border-0 shadow">';
+// $config["menu_header_anchor"]       = '<a role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="nav-link dropdown-toggle bg-birubcas" href="%s">%s</a>';
+// $config["item_header_anchor"]       = '<a role="button" class="nav-link bg-birubcas" href="%s">%s</a>';
+
+// $config["item_tag_open"]            = '<li class="nav-item ml-1">';
